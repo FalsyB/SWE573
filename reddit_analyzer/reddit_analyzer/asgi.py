8cc421417c08dadd7188ba3018d655a226cfb33a
burak-erucar <burak@atlas-robotics.com>
2021-02-02 20:06:09 +0300

    Some markdowns. Realized the files were in the wrong place thanks to the final exam, which i'm doing rn

--- reddit_analyzer/reddit_analyzer/asgi.py
+++ reddit_analyzer/reddit_analyzer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SMAProject project.
+ASGI config for reddit_analyzer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMAProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddit_analyzer.settings')
 
 application = get_asgi_application()
